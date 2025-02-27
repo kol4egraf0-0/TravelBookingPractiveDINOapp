95faa644ad8dbfaca999a5175235683c740e8437
Evgen <genitya408@gmail.com>
2025-02-27 15:04:46 +0300

    lib update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,4 +40,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("com.github.ismaeldivita.chip-navigation-bar:1.4.0")
+    implementation("androidx.viewpager2:viewpager2:1.0.0")
+    implementation("com.github.bumptech.glide:glide:4.12.0")
 }
