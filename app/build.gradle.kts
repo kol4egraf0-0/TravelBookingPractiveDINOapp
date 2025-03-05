7198d66b7ed8f042fd7ea15ffbb8d23a38d5a07e
Evgen <genitya408@gmail.com>
2025-03-05 10:04:10 +0300

    firebase auth commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.firebase.database)
+    implementation(libs.firebase.auth)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
@@ -53,4 +54,6 @@ dependencies {
     implementation("com.github.bumptech.glide:glide:4.12.0")
 
     implementation("com.journeyapps:zxing-android-embedded:4.3.0")
+
+    implementation ("com.google.firebase:firebase-auth:22.1.2")
 }
--- app/src/main/java/com/example/travelbookingapp/Activity/SignActivity.java
+++ app/src/main/java/com/example/travelbookingapp/Activity/SignActivity.java
@@ -1,20 +1,43 @@
 package com.example.travelbookingapp.Activity;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import com.example.travelbookingapp.databinding.ActivitySignBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
-import com.example.travelbookingapp.R;
-
-public class SignActivity extends AppCompatActivity {
+public class SignActivity extends BaseActivity {
+    ActivitySignBinding binding;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign);
+        binding = ActivitySignBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        auth = FirebaseAuth.getInstance();
+
+
+
+        binding.imageView8.setOnClickListener(view -> {
+            String email = binding.editTextTextEmailAddress.getText().toString();
+            String password = binding.editTextTextPassword.getText().toString();
+
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Заполните все поля", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            auth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(this, "Регистрация успешна!", Toast.LENGTH_SHORT).show();
+                            finish();
+                        } else {
+                            Toast.makeText(this, "Ошибка: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        });
     }
 }
